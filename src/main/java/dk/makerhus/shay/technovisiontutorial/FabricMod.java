ce87e9c24b115fe6611fc284e6be2c7d712d848d
Purple CRAFT <purple@makerhus.dk>
2023-02-02 13:47:52 +0100

    Setup workspace

---
+++ src/main/java/dk/makerhus/shay/technovisiontutorial/FabricMod.java
@@ -0,0 +1,13 @@
+package dk.makerhus.shay.technovisiontutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricMod implements ModInitializer {
+
+    public static final String MOD_ID = "shaysfm";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
